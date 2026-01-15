f2d01e374a2f5f7c670cc5b9ea2795c780130eec
nchlnnmrvnz <avanez.nichole@gmail.com>
2026-01-15 15:44:39 +0800

    initial commit

---
+++ src/main/java/dandannnnn/invite_api/InviteApiApplication.java
@@ -0,0 +1,13 @@
+package dandannnnn.invite_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InviteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dandannnnn/invite_api/InviteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dandannnnn.invite_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
